f4110166e8d4c7313182ec34a4af26106dfb442d
stosazuw <steve.osazuwa@agilent.com>
2016-02-18 20:05:48 -0800

    Progressed to collision handling

--- ballFactory.js
+++ ballFactory.js
@@ -4,6 +4,7 @@
 
 	var g = 9.8;  // Acceleration due to gravity, check if this is the proper way to instantiate a constant
 
+	//radius will always be a positive number
 	var Ball = function( initparams ){
 		this.x_curr = initparams.x;
 		this.y_curr = initparams.y;
@@ -14,6 +15,8 @@
 		//TODO potential energy (Needs to be aware of external object, maybe don't handle it here)
 		this.dx = 0;
 		this.dy = 0;
+		this.ddx = initparams.ddx || 0; //acceleration on ball X dir (Should ball contain X,Y accel this i think so)
+		this.ddy = initparams.ddy || 0; //acceleration on ball Y dir (Remember canvas coor (x,-y) not normal cartersian)
 	};
 
 	//Will be exposed to the browser.
@@ -24,22 +27,13 @@
 	};
 
 	//Simulation engine
-	Ball.prototype.updateSimulation = function(dt) {
-	        // F = mg for gravity pulling down a point mass. We equate that force
-	        // with ma to find acceleration and get:
-	        // F = mg = ma --> g = a
-	        var ddx = 0;
-	        var ddy = -g;
-	        // ^ when we're handling collisions, there will be more terms
-	        this.dy -= ddy * dt;
+	Ball.prototype.updateMotion = function(dt) {
+	        //ddx/ddy signs will reflect direction
+	        this.dy += this.ddy * dt;
 	        this.y_curr += this.dy * dt;
 	        //Below dx is 0 at all times in ball drop down simulation
-	        this.dx -= ddx * dt;
+	        this.dx -= this.ddx * dt;
 	        this.x_curr += this.dx * dt;
-
-	        // Collision detection against the ground
-	        //handle via events, pretty important
-
 	        //update Ball Kinetic energy. is this correct form for .5mv^2?  double check its right when dx != 0
 	        this.KineticEnergy = 0.5 * this.m * (this.dx * this.dx + this.dy * this.dy);
 	        //TODO handle potential energy
@@ -61,13 +55,31 @@
 	//  How big that system is though can be were I reduce the calulations I do.  
 	//		Broad phase: Quad Trees
 	//Returns bool true if balls are colliding
-
-	Ball.prototype.CircleCollision = function(circle){
+	//In the future return a vector to apply to the ball as a result of the collision
+	Ball.prototype.IsCircleCollision = function(circle){
 		//Currently objects touching exactly are considered colliding.
 		var Xdist = Math.abs(this.x_curr - circle.x_curr);
 		var Ydist = Math.abs(this.y_curr - circle.y_curr);		
 		var distanceOfCenters = Math.sqrt(Math.pow(Xdist,2) + Math.pow(Ydist,2));
 		return (distanceOfCenters <= (this.radius + circle.radius));
+	};
+
+	//Take a rect and see if the the ball is colliding with any of botders
+	//	For now return true if there is a collision.
+	//	In the future if colliding return a vector
+	//Note the ball has to be in the box for this collision detection to be valid
+	//Issues arise for negative width or height in rect
+	Ball.prototype.StationaryRectangleBorderCollision = function (rect) {
+		var rightSideRect = Math.max(rect.x + rect.width,rect.x);
+		var leftSideRect = Math.min(rect.x + rect.width,rect.x);
+		var bottSideRect = Math.max(rect.y + rect.height, rect.y);
+		var topSideRect = Math.min(rect.y + rect.height, rect.y);
+
+		var x_InRectRange = (this.x_curr + this.radius >= rightSideRect) ||
+							(this.x_curr - this.radius <= leftSideRect);
+		var y_InRectRange = (this.y_curr + this.radius >= bottSideRect) ||
+							(this.y_curr - this.radius <= topSideRect);
+		return (x_InRectRange || y_InRectRange);
 	};
 
 	//Attach ball factory to the window.  Curious what browsers this doesnt work on (Check)
--- canvasState.js
+++ canvasState.js
@@ -2,7 +2,6 @@
 
 var canvas=$("#canvas")[0];
 var ctx=canvas.getContext("2d");
-console.log(ctx);
 //initial state
 // For now balls to be used will be set to 5.  Consider making dynamic and allowing user to choose (For the name portion)
 var State = {
@@ -11,9 +10,11 @@ var State = {
 	dt: 0.2
 };
 State.balls = [];
-State.room = {};
-State.room.width = 300;
-State.room.height = 300;
+State.room = {}; //room in this case is a rectangle
+State.room.x = 0;
+State.room.y = 0;
+State.room.width = canvas.width;
+State.room.height = canvas.height;
 
 //Test region.  Add 5 ball
     for (var i = 0; i < 5; i++) {
@@ -23,15 +24,37 @@ State.room.height = 300;
 		newBall.mass = 1;
 		newBall.radius = 20;
 		newBall.color = "red";
+		newBall.ddy = 9.8;
         State.balls.push(BallFactory(newBall));
     }
 //End test region
+
+//Collisions are elastic.  Preserved E and p
+//This method takes 2 shapes and calculates their new x,y velocity then applies it
+//if there is no shape 2 then the collision is assumed to be with a fixed obj
+function collisionHandle(shape1, shape2){
+	shape2 = shape2 || null;
+	if (shape2 === null) {
+		//bounce:  Find point of collision for obj1 reverse the velocity vector and apply it
+
+	}
+	else{//Handle an elastic collision}
+}
+
+//Returns point on a circle where collision with a fixed obj first occured
+function findCollisionWithFixedObj(circle){
+	//Find direction of collision
+	//Find point on circle arc that is radius away
+	//See how far iver
+}
+
 
 function loop(){
 	//change State - Simulation engine call for next time point
 	for (i = 0, length = State.balls.length; i < length; i++) {
 	var currBall = State.balls[i];
-	currBall.updateSimulation(State.dt);
+	currBall.updateMotion(State.dt);
+	if (currBall.StationaryRectangleBorderCollision(State.room)) { currBall.y_curr = 40; }
 	}
 	//Clear Canvas
 	ctx.fillStyle = "rgb(200,200,200)";
@@ -47,7 +70,5 @@ function loop(){
 setInterval(loop, 50);
 
 //Expose the State in the future for user interaction
-
-
 
 })();
